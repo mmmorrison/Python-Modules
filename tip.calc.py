ce40ee4d533caa0db483123cfe069827be843db4
Maria <mmorrison1978@gmail.com>
2016-03-21 19:00:36 -0600

    updated

---
+++ tip.calc.py
@@ -0,0 +1,15 @@
+class TipCalc():
+	def __init__(self, gratuity=.2):
+		self.tip = tip
+		self.tip_array = []
+
+		def get_total_tips(self):
+			return reduce(lamda x, y: x + y, self.tip_array)
+
+	def add_to_total(self, bill):
+		self.tip_array.append(self.tip * bill)
+		total = self.tip * bill
+		return total
+
+text_tc = TipCalc()
+print test_tc.add_to_total(100)
